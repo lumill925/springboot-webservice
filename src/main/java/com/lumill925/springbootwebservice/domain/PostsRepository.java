9818aabf6a6373df0081c4797af162948ee05f5c
Jungmin-Noh <98960235+lumill925@users.noreply.github.com>
2022-08-01 14:01:12 +0900

    Delete PostsRepository.java
    중복 파일 제거

--- src/main/java/com/lumill925/springbootwebservice/domain/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lumill925.springbootwebservice.domain;
-
-import com.lumill925.springbootwebservice.domain.posts.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
